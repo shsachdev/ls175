8f11967beccccf74a6049fa1beb333f1df58abb6
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-08 15:58:14 -0800

    dynamic text, courtersy erb files

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -1,6 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
+require "tilt/erubis"
 
 get "/" do
-  File.read "public/template.html"
+  @title = "The Adventures of Sherlock Holmes"
+  erb :home
 end
