5d737eb900d9af60a843ec542987f40eaacb82e6
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-08 17:15:15 -0800

    different style sheets

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -10,5 +10,9 @@ get "/" do
 end
 
 get "/chapters/1" do
-  @title = 
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
+  erb :chapter
 end
