fd2a6a6957939ec2148cc1daccd8cb94dc25fb60
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-10 18:11:43 -0800

    sort list of fiels array desc

--- dynamic_directory_index/app.rb
+++ dynamic_directory_index/app.rb
@@ -7,6 +7,16 @@ get "/" do
   erb :main_listing
 end
 
+get "/sort/ascending" do
+  @list_of_files = Dir.glob("public/*")
+  erb :main_listing
+end
+
+get "/sort/descending" do
+  @list_of_files = Dir.glob("public/*").sort {|a,b| b <=> a}
+  erb :main_listing
+end
+
 def file_cleaner(file)
   file.split("/")[1]
 end
