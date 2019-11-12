673f9c39c5088fd3a27491b3d2e9718bdf5bb356
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-11 19:01:38 -0800

    generalized chap number in url

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -9,12 +9,20 @@ get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+# get "/chapters/1" do
+#   @title = "Chapter 1"
+#   @contents = File.readlines("data/toc.txt")
+#   @chapter = File.read("data/chp1.txt")
+#
+#   erb :chapter
+# end
 
-  erb :chapter
+get "/show/:name" do
+  params[:name] # params hash is automatically made available in routes
 end
 
-get "/chapters"
+get "/chapters/:number" do
+  @title = "Chapter #{params[:number]}"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp#{params[:number]}.txt")
+end
