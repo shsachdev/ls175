a29533977740396981f095e036dc9b6d7d566a67
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-13 21:50:11 -0800

    setting up user profiles

--- users_and_interests/main_app.rb
+++ users_and_interests/main_app.rb
@@ -3,11 +3,19 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require "yaml"
 
+before do
+  @content = YAML.load_file('users.yaml')
+end
+
 get "/" do
   redirect "/users"
 end
 
 get "/users" do
-  @content = YAML.load_file('users.yaml')
   erb :users
+end
+
+get "/users/:name" do
+  
+  erb :individual_user
 end
