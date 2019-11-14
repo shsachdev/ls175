78309e3ca928756bfc8fb83a9e3a6050df69aa23
Shikhar Sachdev <shsachdev@gmail.com>
2019-11-13 21:33:01 -0800

    loading data of yaml file into erb file

--- users_and_interests/main_app.rb
+++ users_and_interests/main_app.rb
@@ -1,12 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "yaml"
 
 get "/" do
   redirect "/users"
 end
 
 get "/users" do
-  @content = File.
+  @content = YAML.load_file('users.yaml')
   erb :users
 end
